a422770fec13a50e2a67ffedb41c85fdff4b2e7d
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2025-09-25 22:25:09 +0400

    commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Ededi daxil edin:");
+int eded = int.Parse(Console.ReadLine());
+
+if (90 < eded && eded <= 100)
+{
+	Console.WriteLine("A");
+}
+else if (80 < eded && eded <= 90)
+{
+	Console.WriteLine("B");
+}
+else if (70 < eded && eded <= 80)
+{
+	Console.WriteLine("C");
+}
+else if (60 < eded && eded <= 70)
+{
+	Console.WriteLine("D");
+}
+else if (50 < eded && eded <= 60)
+{
+	Console.WriteLine("E");
+}
+else if (0 < eded && eded <= 50)
+{
+	Console.WriteLine("F");
+}
+
