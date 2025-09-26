875c19fc72581ff8f3ed95204cfbc9802be06910
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2025-09-26 21:43:58 +0400

    commit

--- Program.cs
+++ Program.cs
@@ -1,32 +1,49 @@
-﻿Console.Write("Ededi daxil edin:");
-int eded = int.Parse(Console.ReadLine());
+﻿
+#region Task1
+//Console.Write("Ededi daxil edin:");
+//int eded = int.Parse(Console.ReadLine());
+//int cem = 0;
 
-if (eded < 0 || eded > 100)
+//for (int i = 0; i <=eded; i++)
+//{
+//	cem=cem + i;
+//}
+//Console.WriteLine(cem);
+#endregion
+
+
+
+#region Task2
+Console.Write("Peşəni daxil edin:");
+string peşə = Console.ReadLine();
+
+Console.Write("Maaşı daxil edin:");
+int maaş = int.Parse(Console.ReadLine());
+
+switch (peşə)
 {
-	Console.WriteLine("Xəta: Bal 0 ilə 100 arasında olmalıdır!");
+	case "Menecer":
+		maaş = maaş * 3;
+		Console.WriteLine($"Menecer = {maaş}AZN");
+		break;
+	case "Proqramci":
+		maaş = maaş * 2;
+		Console.WriteLine($"Proqramci = {maaş}AZN");
+		break;
+	case "Dizayner":
+		maaş = maaş + 500;
+		Console.WriteLine($"Dizayner = {maaş} AZN");
+		break;
+	case "Temirci":
+		maaş = maaş - 200;
+		Console.WriteLine($"Təmirci = {maaş} AZN");
+		break;
+
+	default:
+		Console.WriteLine("Belə bir peşə tapılmadı.");
+		break;
 }
-if (90 < eded && eded <= 100)
-{
-	Console.WriteLine("A");
-}
-else if (80 < eded && eded <= 90)
-{
-	Console.WriteLine("B");
-}
-else if (70 < eded && eded <= 80)
-{
-	Console.WriteLine("C");
-}
-else if (60 < eded && eded <= 70)
-{
-	Console.WriteLine("D");
-}
-else if (50 < eded && eded <= 60)
-{
-	Console.WriteLine("E");
-}
-else if (0 < eded && eded <= 50)
-{
-	Console.WriteLine("F");
-}
+
+#endregion
+
 
